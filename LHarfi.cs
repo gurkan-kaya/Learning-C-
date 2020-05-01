0796e0104055b5bd7b5b4c467a3fa51861d94f66
gurkan-kaya <56433592+gurkan-kaya@users.noreply.github.com>
2020-05-01 17:13:26 +0300

    Create LHarfi.cs
    W A S D İLE HAREKET EDEN ŞEKİL

---
+++ LHarfi.cs
@@ -0,0 +1,68 @@
+using System;
+namespace ConsoleApp7
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           LHarfi harf = new LHarfi();
+            while(true)
+            {
+               harf.Goster();
+                ConsoleKeyInfo tus = Console.ReadKey();
+                if (tus.Key == ConsoleKey.A)
+                    harf.X--;
+                if (tus.Key == ConsoleKey.D)
+                    harf.X++;
+                if (tus.Key == ConsoleKey.W)
+                    harf.Y--;
+                if (tus.Key == ConsoleKey.S)
+                    harf.Y++;
+
+
+
+            }
+
+        }
+
+    }
+     class LHarfi
+    { 
+          public int X,Y;
+          private int yukseklik;
+          private int genislik;
+        public LHarfi()
+        {
+            Y = 10; 
+            X = 25;
+            yukseklik =15;
+            genislik =20;
+        }
+
+        public void Goster()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            for (int i = 0; i < yukseklik; i++)
+            {
+                Console.SetCursorPosition(X, Y + i);
+                Console.Write(" ");
+
+            }
+            for (int i = 0; i < genislik; i++)
+            {
+                Console.SetCursorPosition(X + i,Y+yukseklik);
+                Console.Write(" ");
+            }
+
+            Console.ResetColor();
+
+        }
+
+
+
+    }
+
+}
