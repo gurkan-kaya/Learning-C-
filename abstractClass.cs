4a1819262e4ec5ae7d56ddfb895e0aba2d2bdc64
gurkan-kaya <56433592+gurkan-kaya@users.noreply.github.com>
2020-06-09 23:32:43 +0300

    Update abstractClass.cs

--- abstractClass.cs
+++ abstractClass.cs
@@ -9,18 +9,29 @@ namespace AbstractClass
     //Public veya private olmalıdır.
     //Abstract sınıflardan nesne oluşturulmaz ancak referans noktası alınabilir.
     //Abstract metot ya da propertylerin sadece signatureleri abstract classta yazılır,gövdeleri yazılmaz.
-    
     abstract class MyClass
     {
-        public abstract void X(); //Sadece imza tanımladık.
+        public abstract void X(); //Sadece imza tanımladık
         public void Y() { } //Abstract olmak zorunda değil.
+    }
+
+    class MyClass2 : MyClass
+    {
+        public override void X()
+        {
+         
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            // MyClass mc = new MyClass();  (Hatalı bir kullanımdır)
 
+            MyClass mc = new MyClass2(); //Abstract referans noktası üzerinden derived classın nesnesine erişim sağladık.(Polimorfizm)
         }
     }
 }
+
+
